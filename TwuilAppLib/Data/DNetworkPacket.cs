ea674424194ba3e53bc3fd63dc7bc5c9a3a360c4
Twan Hogervorst <twanhog@gmail.com>
2020-10-13 15:03:42 +0200

    Added Network Packages

---
+++ TwuilAppLib/Data/DNetworkPacket.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwuilAppLib.Data
+{
+    public class DNetworkPacket : DAbstract
+    {
+        public string type;
+        [JsonProperty]
+        private JObject data;
+
+        public DNetworkPacket<T> DataAsType<T>() where T : DAbstract
+        {
+            return new DNetworkPacket<T>()
+            {
+                type = this.type,
+                data = data?.ToObject<T>()
+            };
+        }
+    }
+
+    public class DNetworkPacket<T> : DAbstract where T : DAbstract
+    {
+        public string type;
+        public T data;
+    }
+
+    public class DLoginPacket : DAbstract
+    {
+        public string username;
+        public string password;
+    }
+
+    public class DMessagePacket : DAbstract
+    {
+        public string sender;
+        public string receiver;
+        public string message;
+    }
+}
