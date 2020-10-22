e617572373ef6f2aaf7a331b56a4682a3a3a00a5
Guilliam Lutz <guilliamlutz@outlook.com>
2020-10-22 11:07:31 +0200

    ClientState implementation

---
+++ TwuilAppClient/ClientState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwuilAppClient
+{
+    interface ClientState
+    {
+
+        bool Login(string username, string password)
+        {
+            return false;
+        }
+
+        void SenPrivateMessage(string username, string message)
+        {
+
+        }
+
+    }
+}
